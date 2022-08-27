47160ee68e55af644ce9b0be8d0587487ed95198
Lee Jaeseok <jsuk8852@naver.com>
2022-08-28 01:13:59 +0900

    냉장고만 보기위해 나머지 문자처리 및 보기 쉽게 하기 위해 rand 잠시 지움 또한 냉장고 임의로 구현해 봄

--- Run_Generate_Dummies.m
+++ Run_Generate_Dummies.m
@@ -2,25 +2,36 @@ clc, clear, close all
 
 %%% #1 On/Off 부하
 
-t2 = -0.1:1/10e3:0.3;
-w =  (60e-3);
-L1 = Func_Onoff(t2-0.1,w);
+%t2 = -0.1:1/10e3:0.3;
+%w =  (60e-3);
+%L1 = Func_Onoff(t2-0.1,w);
 
 %%% #2 순시(Puls) 부하
 
-t3 = -0.1:1/10e3:0.3;
-w2 =  1e-3;
-L2 = Func_Onoff(t2,w2);
+%t3 = -0.1:1/10e3:0.3;
+%w2 =  1e-3;
+%L2 = Func_Onoff(t2,w2);
+
 %%% #3 주기성 부하
-t = -0.1:1/10e3:0.3;
-cycle = 2*pi*30;
-duty = 37;
+t = 0:1:1100;
+%밤주기
+if t<=200
+   cycle = 200;
+   duty = 62.5; %%66.67
+%낮주기
+elseif 200<t<=700
+    cycle = 250;
+    duty=66.67;
+%밤주
+elseif t>700
+    cycle = 200;
+     duty = 62.5;
+end
 L3 = Func_Signal_Cycle(t, cycle, duty);
 
 %%종합
-L4= L1+L2+L3;
+%L4= L1+L2+L3;
 
-%%% #4 인버터 부하
 
 %%t
-plot(t2, L1,t ,L3,t3,L2,t,L4)
+plot(t ,L3)
