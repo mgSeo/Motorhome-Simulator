c25a193330d34047be99a017c3708028d1d2dab0
jseok9905 <94904597+jseok9905@users.noreply.github.com>
2022-08-30 22:17:53 +0900

    Add files via upload

--- Run_Generate_Dummies.m
+++ Run_Generate_Dummies.m
@@ -2,25 +2,37 @@ clc, clear, close all
 
 %%% #1 On/Off 부하
 
-t2 = -0.1:1/10e3:0.3;
-w =  (60e-3);
-L1 = Func_Onoff(t2-0.1,w);
+%t2 = -0.1:1/10e3:0.3;
+%w =  (60e-3);
+%L1 = Func_Onoff(t2-0.1,w);
 
 %%% #2 순시(Puls) 부하
 
-t3 = -0.1:1/10e3:0.3;
-w2 =  1e-3;
-L2 = Func_Onoff(t2,w2);
+%t3 = -0.1:1/10e3:0.3;
+%w2 =  1e-3;
+%L2 = Func_Onoff(t2,w2);
+
 %%% #3 주기성 부하
-t = -0.1:1/10e3:0.3;
-cycle = 2*pi*30;
-duty = 37;
+%밤주기
+
+t = 1:10000;
+cycle = 0.000967;
+duty = 65.67;
 L3 = Func_Signal_Cycle(t, cycle, duty);
 
-%%종합
-L4= L1+L2+L3;
+%모터 모드
+%t5= 1:1000;
+%prompt="mode?";
+%k=input(prompt);
+%cycle2=modevalue(k);
+%duty2=100*dutyvalue(k);
 
-%%% #4 인버터 부하
+%L5=Func_Signal_Cycle(t5, cycle2, duty2);
+
+%%종합
+%L4= L1+L2+L3;
+
 
 %%t
-plot(t2, L1,t ,L3,t3,L2,t,L4)
+%plot(t5 ,L5)
+plot(t,L3)
---
+++ modevalue.m
@@ -0,0 +1,4 @@
+function u = modevalue(r)
+
+u = 2*pi/((r+6)*10);
+end
